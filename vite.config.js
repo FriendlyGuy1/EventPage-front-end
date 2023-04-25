072f405ba74969f2980529fad1299eaefbe6eea5
D.Navickas <112947625+DovydasNavickas0@users.noreply.github.com>
2023-04-25 20:56:57 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({ //if somebody touches this there a dead man
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': 'http://localhost:7050'
+      '/api': 'https://eventpage.onrender.com'
     },
   },
   plugins: [react()],
