9405c12d3e72f6ecd074c468d7a4a2ddf3e31a04
Paulius Savickas <pauliussavickas18@gmail.com>
2023-05-16 03:14:52 +0300

    added changes for Production

--- src/features/image/imageService.js
+++ src/features/image/imageService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/uploadImage/'
+const API_URL = process.env.NODE_ENV === "production" ? "https://eventpage.onrender.com/api/uploadImage/" : '/api/uploadImage/'
 
 
 const uploadSingleImage = async (base64) => {
